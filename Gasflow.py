f85c2812695b5855dcf76f1918ba67f40831faf6
smoothcriminal <dim.zaycev@gmail.com>
2017-08-15 18:11:47 +0300

    ...

--- Gasflow.py
+++ Gasflow.py
@@ -1,10 +1,18 @@
 import json
+from math import *
+gas_viscosity = 14.3 * 10 ** -6 # standard conditions
+gas_density = 0.73 # standard conditions
+gas_temp = 273.5
+roughness = 0.1
+air_temp = 293.15
 
 class Node(object):
 
-    def __init__(self, id, incidents):
+    def __init__(self, id, incidents, q, p):
         self._id = id
         self.incidents = incidents
+        self.q = q
+        self.p = p
 
     def __str__(self):
         return 'Node'
@@ -16,9 +24,7 @@ class Consumer(Node):
         p - in pressure
     """
     def __init__(self, data):
-        Node.__init__(self, data['_id'], data['incidents'])
-        self.p = data['p'] if data['p'] else data['p']
-        self.q = data['q'] if data['q'] else data['q']
+        Node.__init__(self, data['_id'], data['incidents'], data['q'], data['p'])
 
     def __str__(self):
         return 'Consumer: Id {} Volume {} Pressure {}'.format(self._id, self.q, self.p)
@@ -30,9 +36,7 @@ class Source(Node):
         p - out pressure
     """
     def __init__(self, data):
-        Node.__init__(self, data['_id'], data['incidents'])
-        self.p = data['p'] if data['p'] else data['p']
-        self.q = data['q'] if data['q'] else data['q']
+        Node.__init__(self, data['_id'], data['incidents'], data['q'], data['p'])
 
     def __str__(self):
         return 'Source: Id {} Volume {} Pressure {}'.format(self._id, self.q, self.p)
@@ -45,7 +49,7 @@ class Pipe(Node):
         r: roughness
     """
     def __init__(self, data):
-        Node.__init__(self, data['_id'], data['incidents'])
+        Node.__init__(self, data['_id'], data['incidents'], 0, 0)
         self.length = data['length']
         self.diameter = data['diameter']
         self.roughness = data['roughness']
@@ -53,13 +57,39 @@ class Pipe(Node):
     def __str__(self):
         return 'Pipe:\nId {}\nLength {}\nDiameter {}'.format(self._id, self.length, self.diameter)
 
+    """ Re number """
+    def getFrictionCoef():
+        re_num = (0.0354 * self.q) / (self.diameter * gas_viscosity)
+        coef = 0
+        if re_num <= 2000: # laminar mode
+            coef = 64 / re_nu
+        elif re_num > 2000 and re_num <= 4000: # critical mode
+            coef = 0.0025 * (re_num * 0.333)
+        else:
+            if re_num * (roughness / self.diameter): # pipe wall smoothness
+                if re_num > 4000 and re_num <= 100000:
+                    coef = 0.3164 / (re_num * 0.25)
+                else:
+                    coef = 1 / ((1.82 * log10(re_num) - 1.64) ** 2)
+            else:
+                coef = 0.11 * ((roughness / self.diameter + 68 / re_num) ** 0.25)
+
+    """ Avg compressibility of gas """
+    def getCompressibilityCoef():
+        a1 = -0.39 +
+        1 +
+
+    def getPressureDrop():
+        fc = getFrictionCoef()
+        4.324 * 10 ** -7 * fc * self.q * abs(self.q) * gas_density * self.length * gas_temp / d ** 5
+
 class Valve(Node):
     """
     Args:
         closed: 1 closed, 0 opened
     """
     def __init__(self, data):
-        Node.__init__(self, data['_id'], data['incidents'])
+        Node.__init__(self, data['_id'], data['incidents'], 0, 0)
         self.closed = bool(data['closed'])
 
     def __str__(self):
@@ -89,8 +119,10 @@ def buildGraph():
 def findPath(_from, to):
     def constructPath(prev):
         path = []
+        q = prev.q
         while prev != None:
             path.append(prev)
+            prev.q += q
             prev = backtrace[prev]
         return path[::-1]
     toVisit = [_from]
@@ -115,6 +147,8 @@ g = buildGraph()
 def findDistribution():
     return [findPath(s, c) for s in sources for c in consumers]
 
+# def calcPressureDrop():
+
 flows = findDistribution()
 for f in flows:
-    print([i._id for i in f])
+    print([(i._id, i.q) for i in f])
